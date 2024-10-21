cb81f4ea761ae87a87b5c30eb2c9ab97bef9079c
Javier Rodríguez <134724418+javierrdlp@users.noreply.github.com>
2024-10-21 18:18:34 +0000

    [CREAR GENERADOR] - index.html modifiqué un poco el archivo y añadí un bottón. - app.js realaicé toda la lógica del generador

--- src/app.js
+++ src/app.js
@@ -6,6 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let paragraph = document.getElementById("excuse");
+  paragraph.innerText = sentencesCreator(who, action, what, when);
 };
+
+function sentencesCreator(arr1, arr2, arr3, arr4) {
+  let arr = [arr1, arr2, arr3, arr4];
+  let positions = [4, 4, 3, 5];
+  let sentence = "";
+  for (let i = 0; i < 4; i++) {
+    sentence =
+      sentence + arr[i][Math.floor(Math.random() * positions[i])] + " ";
+  }
+
+  return sentence;
+}
